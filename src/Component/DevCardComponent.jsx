15ef9ed5e7ecd6b001a4b8d27380183ff1dc9a43
thibault hercod <hercod.thibault@gmail.com>
2023-11-14 18:06:32 +0100

    add employed tech

--- src/Component/DevCardComponent.jsx
+++ src/Component/DevCardComponent.jsx
@@ -15,21 +15,21 @@ function DevCardComponent({image, alt, job, name, location, text, link1, link2})
                         <div className="w-full text-center mt-20">
                             <div className="flex justify-center lg:pt-4 pt-8 pb-0">
                                 <div className="p-3 text-center">
-                                    <p>{job}</p>
+                                    <p className="text-textColor1">{job}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="text-center mt-2">
-                        <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1">{name}</h3>
+                        <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1 text-textColor2">{name}</h3>
                         <div className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase">
-                            <i className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75"></i>{location}
+                            <i className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75 text-textColor1"></i>{location}
                         </div>
                     </div>
                     <div className="mt-6 py-6 border-t border-slate-200 text-center">
                         <div className="flex flex-wrap justify-center">
                             <div className="w-full px-4">
-                                <p className="font-light leading-relaxed text-slate-600 mb-4">{text}</p>
+                                <p className="font-light leading-relaxed mb-4 text-textColor1">{text}</p>
                                 <div className="flex flex-row">
                                     <a href={link1} className="w-20 h-20 hover:text-slate-400"><FaGithubSquare className="w-10 h-10"/></a>
                                     <a href={link2} className="w-20 h-20 hover:text-slate-400"><FaLinkedin className="w-10 h-10"/></a>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,41 +1,76 @@
 import Navbar from "../Component/Navbar"
 import Footer from "../Component/Footer"
 import DevCardComponent from "../Component/DevCardComponent"
+import { FaReact } from "react-icons/fa";
+import { SiVite } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
 
 
 function About() {
     return (
         <div className="flex flex-col min-h-screen">
-            <Navbar />
-            <div className="mb-auto flex flex-center justify-around items-stretch mt-16 m-4">
-                <div>
-                    <DevCardComponent
-                        image="public/_d31081e1-769d-48c5-88d6-1ffcd8b196de.jfif"
-                        alt="avatar"
-                        job="Data Analyste"
-                        name='Vincent Maton'
-                        location="Belgique - Saint-Ghislain"
-                        text="DATA Analyste de renom et co-auteur de la plateforme future forge AI."
-                        link1="https://github.com/VinceM77"
-                        link2="https://www.linkedin.com/in/vincent-maton-6b8b9b1b0/"
-                    />
-                </div>
-                <div>
-                    <DevCardComponent
-                        image="public/_e7d0171d-36ed-48fe-8977-6fdb849a4432.jfif"
-                        alt="avatar"
-                        job="Developeur Web"
-                        name='Thibault Hercod'
-                        location="Belgique - Bruxelles"
-                        text="Web developer et co-auteur de la plateforme future forge AI."
-                        link1="https://github.com/ThibaultRko"
-                        link2="https://www.linkedin.com/in/thibault-hercod"
-                    />
-                </div>
-            </div>
-            
-            <Footer className="w-screen fixed bottom-0" />
+    <Navbar />
+    <div className="mb-auto grid grid-cols-2 grid-rows-2 gap-4 mt-16 m-4">
+        <div className="p-8">
+            <DevCardComponent
+                image="public/_d31081e1-769d-48c5-88d6-1ffcd8b196de.jfif"
+                alt="avatar"
+                job="Data Analyste"
+                name='Vincent Maton'
+                location="Belgique - Saint-Ghislain"
+                text="DATA Analyste de renom et co-auteur de la plateforme future forge AI."
+                link1="https://github.com/VinceM77"
+                link2="https://www.linkedin.com/in/vincent-maton-6b8b9b1b0/"
+            />
         </div>
+        <div className="pt-12">
+            <h2 className="flex justify-start text-4xl font-bold text-startCargusHover">EMPLOYED TECHNOLOGIES</h2>
+            <div className="flex flex-row justify-between">
+                <p className="flex flex-col content-start items-start text-4xl font-bold italic mt-4 text-textColor2">REACT</p>
+                <p><FaReact className="w-10 h-10 mt-4 mr-32"/></p>
+            </div>
+            <div className="flex flex-row justify-between">
+                <p className="flex flex-col content-start items-start text-4xl font-bold italic text-textColor2 mt-4">VITE</p>
+                <p><SiVite className="w-10 h-10 mt-4 mr-32"/></p>
+            </div>
+            <div className="flex flex-row justify-between">
+                <p className="flex flex-col content-start items-start text-4xl font-bold italic text-textColor2 mt-4">JAVASCRIPT</p>
+                <p><IoLogoJavascript className="w-10 h-10 mt-4 mr-32"/></p>
+            </div>
+        </div>
+        <div className="pt-12">
+            <h2 className="flex justify-end text-4xl font-bold text-startCargusHover">EMPLOYED TECHNOLOGIES</h2>
+            <div className="flex flex-row justify-between">
+                <p><FaReact className="w-10 h-10 mt-4 ml-32"/></p>
+                <p className="flex flex-col content-end items-end text-4xl font-bold italic mt-4 text-textColor2">REACT</p>
+            </div>
+            <div className="flex flex-row justify-between">
+                <p><SiVite className="w-10 h-10 mt-4 ml-32"/></p>
+                <p className="flex flex-col content-end items-end text-4xl font-bold italic text-textColor2 mt-4">VITE</p>
+            </div>
+            <div className="flex flex-row justify-between">
+                <p><IoLogoJavascript className="w-10 h-10 mt-4 ml-32"/></p>
+                <p className="flex flex-col content-end items-end text-4xl font-bold italic text-textColor2 mt-4">JAVASCRIPT</p>
+            </div>
+        </div>
+        <div className="p-8">
+            <DevCardComponent
+                image="public/_e7d0171d-36ed-48fe-8977-6fdb849a4432.jfif"
+                alt="avatar"
+                job="Developeur Web"
+                name='Thibault Hercod'
+                location="Belgique - Bruxelles"
+                text="Web developer et co-auteur de la plateforme future forge AI."
+                link1="https://github.com/ThibaultRko"
+                link2="https://www.linkedin.com/in/thibault-hercod"
+            />
+        </div>
+    </div>
+    <Footer className="w-screen fixed bottom-0" />
+</div>
+
+
+
     )
 }
 export default About
