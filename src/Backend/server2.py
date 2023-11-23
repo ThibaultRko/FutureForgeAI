1cc6328f7b72c848f47ef0f88fc3eb5a30f8718b
VinceM77 <vinzmdark@gmail.com>
2023-11-23 12:30:13 +0100

    Adding CNN model and data transformation to fit the model all results are console logged

--- src/Backend/server2.py
+++ src/Backend/server2.py
@@ -2,17 +2,19 @@ from flask import Flask, request, jsonify
 import numpy as np
 import joblib
 import pandas as pd
-from flask_cors import CORS
 from flask import Flask, request, jsonify
-from keras.models import load_model
+from flask_cors import CORS
 import tensorflow as tf
+from keras.models import load_model
+import cv2
+
 
 app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-model = joblib.load('src/Backend/BestUsedCar2.pkl')
-model2 = load_model('src/Backend/mon_model.keras')
+model = joblib.load('.\\BestUsedCar2.pkl')
+
 
 @app.route('/process', methods=['POST'])
 def process():
@@ -41,12 +43,73 @@ def process():
     return jsonify({'prediction': prediction.tolist()})
 
 ##################################### CHAR REQUEST ################################################
+model2 = load_model('mon_modele.h5')
 
 @app.route('/char', methods=['POST'])
 def char():
-    data = request.get_json(force=True)
-    coordinates = data['coordinates']
+    data2 = request.get_json(force=True)
+    coordinates = data2['coordinates']
     print("coordinates : ", coordinates)
+
+    # Créez un tableau de dimension 300x300 rempli de 1
+    tableau = np.ones((300, 300))
+
+    # Supposons que 'd' est la liste de coordonnées reçues par l'application web
+    for sublist in coordinates:
+        for point in sublist:
+            x = int(float(point['x']))
+            y = int(float(point['y']))
+
+        # coordonnées sont dans les limites du tableau
+            if 0 <= x < 300 and 0 <= y < 300:
+                # Dessinez un petit cercle autour de chaque point pour épaissir le trait 
+                cv2.circle(tableau, (x, y), radius=12, color=(0,0,0), thickness=-1) 
+
+# Redimensionnez du tableau pour correspondre à l'entrée attendue par notre modèle
+    resized_tableau = cv2.resize(tableau, (256,192))  # Largeur x Hauteur
+
+# si le tableau est en niveaux de gris, on ajoute une dimension supplémentaire pour représenter le canal de couleur
+    if len(resized_tableau.shape) == 2:  # évaluer si niveau de gris
+        resized_tableau = np.expand_dims(resized_tableau, axis=-1)
+
+
+# Si votre modèle attend une entrée de forme (256, 192, 1), vous devez ajouter une dimension supplémentaire à votre tableau pour représenter le batch size
+    resized_tableau = np.expand_dims(resized_tableau, axis=0)
+
+# Effectuez une prédiction avec votre modèle
+    predictions = model2.predict(resized_tableau)
+
+    print("predictions : ", predictions)
+
+    class_labels = np.argmax(predictions, axis=1)
+
+    # Obtenez les indices des 5 meilleurs labels
+    top_5_indices = np.argsort(predictions[0])[-5:][::-1]
+
+    print("top_5_indices : ", top_5_indices)
+
+#  liste de labels correspondant à vos indices
+    labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
+        'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c',
+        'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+    top_5_labels = [labels[i] for i in top_5_indices]
+
+    print("top_5_labels : ", top_5_labels)
+
+# Obtenez les probabilités des 5 meilleurs labels
+    top_5_probabilities = predictions[0][top_5_indices]
+
+# Convertissez les probabilités en pourcentages
+    top_5_percentages = top_5_probabilities * 100
+
+# Affichez les labels et leurs pourcentages correspondants
+    for label, percentage in zip(top_5_labels, top_5_percentages):
+        print(f"{label}: {percentage:.2f}%")
+
+    # Retourner la prédiction comme sélection JSON
+    return jsonify({'prediction': predictions.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
